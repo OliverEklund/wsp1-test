2f6c474360506e5e220950a1a4648be6a4cf935a
OliverEklund <oliver.eklund@elev.ga.ntig.se>
2025-08-27 10:59:26 +0200

    thank you twin

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import express from "express"
+// let, var, 
 
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("hello world!")
+    res.send("<h1>Hello World!<h1>")
+} )
+
+app.get("/about", (req, res) => {
+   res.json({
+    "message": "Evil"
+   })
 } )
 
 app.listen(3000, () => {
-    console.log("server is running on port 3000")
+    console.log("server is running on http://localhost:3000")
 })
